957fa06af8908e923b313e82bd3f2fe71e49907d
bunyangpro <bunyangpro2@gmail.com>
2026-05-14 11:23:27 +0900

    수정

--- hobansummit_siheung/form.js
+++ hobansummit_siheung/form.js
@@ -5,21 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
 
-      const scriptURL = "https://script.google.com/macros/s/AKfycbyldEV8bkh10vX4mr9FR0nT1igcey2gjfDEFyFFZfgmauuCp12mV5K8srYJratRhpmh/exec";
+      const scriptURL = "https://script.google.com/macros/s/AKfycbxmk6BlESknhkNkhH-ANhglYJQLGDx6FZ7ZTmJ4XfaPC4tNlHPcV0CxDaS13oWjlMARCQ/exec";
 
       const formData = new FormData(form);
 
       fetch(scriptURL, {
         method: "POST",
-        body: formData
-      })
-        .then(() => {
-          alert("예약 신청이 완료되었습니다.");
-          form.reset();
-        })
-        .catch(() => {
-          alert("전송 오류가 발생했습니다.");
-        });
+        body: formData,
+        mode: "no-cors"
+      });
+
+      alert("예약 신청이 완료되었습니다.");
+      form.reset();
     });
   });
 });
