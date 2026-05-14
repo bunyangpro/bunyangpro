eb4b76ee2746e4a8d829889e5cd7f22ecf182a26
bunyangpro <bunyangpro2@gmail.com>
2026-05-14 13:34:13 +0900

    시트변경

--- hobansummit_siheung/form.js
+++ hobansummit_siheung/form.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
 
-      const scriptURL = "https://script.google.com/macros/s/AKfycbz9vFffXvym60_1gDqW7_RbWBPyoI9pKCdf6Ah4ciUnNWEtsgWYj28McQ7GC_L8Q81aJg/exec";
+      const scriptURL = "https://script.google.com/macros/s/AKfycbxricHmjphLQRHTnCA8eNPFkWF4zQxWkEdY8nm-reuxE_sFYe2ZhvbRUKShH3jzEU4A/exec";
 
       const formData = new FormData(form);
 
