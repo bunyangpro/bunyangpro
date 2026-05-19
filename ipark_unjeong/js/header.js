e1c973feaf20641a0e0c2a60af11d08f73b5d343
bunyangpro <bunyangpro2@gmail.com>
2026-05-19 09:56:43 +0900

    이벤트 팝업 추가

--- ipark_unjeong/js/header.js
+++ ipark_unjeong/js/header.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // 이벤트 팝업 배너
+  const popup = document.getElementById("eventPopup");
+  const closeBtn = document.getElementById("eventClose");
+
+  closeBtn.addEventListener("click", function () {
+    popup.classList.add("hide");
+  });
+
+  popup.addEventListener("click", function (e) {
+    if (e.target === popup) {
+      popup.classList.add("hide");
+    }
+  });
+
+
+  // 헤더 스크롤 이벤트 
   const header = document.getElementById("siteHeader");
 
   window.addEventListener("scroll", function () {
