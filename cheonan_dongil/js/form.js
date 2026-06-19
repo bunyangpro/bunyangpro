25d5b810d99532094cb9f5c6efb5425a616cd8e7
bunyangpro <bunyangpro2@gmail.com>
2026-06-19 09:47:11 +0900

    랜딩 form 방식 변경, js 수정,  complete 폴더 및 파일들 추가 - 천안 동일하이빌

--- cheonan_dongil/js/form.js
+++ cheonan_dongil/js/form.js
@@ -1,25 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*폼 제출시 시트로 전달*/
-    const forms = document.querySelectorAll("#reserveForm");
 
-    forms.forEach(function (form) {
-      form.addEventListener("submit", function (e) {
-        e.preventDefault();
+  const forms = document.querySelectorAll("#reserveForm");
 
-        const scriptURL = "https://script.google.com/macros/s/AKfycbwr7rtrF024xwtrw4Lmxx9Ba0g0DStHex_c_REnuUxyWt2LfKnID-CFg7oFtBfES3Ju3g/exec";
+  forms.forEach(function (form) {
 
-        const formData = new FormData(form);
+    form.addEventListener("submit", function (e) {
 
-        fetch(scriptURL, {
-          method: "POST",
-          body: formData,
-          mode: "no-cors"
-        });
+      e.preventDefault();
 
-        alert("예약 신청이 완료되었습니다.");
+      const scriptURL = "https://script.google.com/macros/s/AKfycbwr7rtrF024xwtrw4Lmxx9Ba0g0DStHex_c_REnuUxyWt2LfKnID-CFg7oFtBfES3Ju3g/exec";
+
+      const formData = new FormData(form);
+
+      fetch(scriptURL, {
+        method: "POST",
+        body: formData,
+        mode: "no-cors"
+      })
+      .then(() => {
+
         form.reset();
+
+        window.location.href = "./complete/";
+
+      })
+      .catch((error) => {
+
+        console.error(error);
+        alert("전송 중 오류가 발생했습니다.");
+
       });
+
     });
 
-    
+  });
+
 });
