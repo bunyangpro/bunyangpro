0a68dcdd4b3d330a696bd8e30b70f505a4b2a5e4
bunyangpro <bunyangpro2@gmail.com>
2026-06-30 15:13:49 +0900

    랜딩페이지 수정

--- hobansummit_siheung2/js/header.js
+++ hobansummit_siheung2/js/header.js
@@ -14,4 +14,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+
+  // 헤더 스크롤 이벤트 
+  const header = document.getElementById("siteHeader");
+  const logo = document.querySelector(".header-logo");
+  const call = document.querySelector(".header-tel img");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+      header.classList.add("scrolled"); logo.src = "./images/header-logo.png"; 
+      header.classList.add("scrolled"); call.src = "./images/call-color.svg";
+    } else {
+      header.classList.remove("scrolled"); logo.src = "./images/header-logo.png";
+      header.classList.remove("scrolled"); call.src = "./images/call.svg"
+    }
+  });
+
 });
