5906ccc941688261dfd9fbf125fdbcade3c415aa
bunyangpro <bunyangpro2@gmail.com>
2026-06-30 15:27:36 +0900

    하늘채 수정

--- eomgung_haneulchae/js/header.js
+++ eomgung_haneulchae/js/header.js
@@ -11,12 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const logo = document.querySelector(".header-logo");
-  
+  const call = document.querySelector(".header-tel img");
+
   window.addEventListener("scroll", () => { 
     if (window.scrollY > 60) { 
       header.classList.add("scrolled"); logo.src = "./images/logo-color.svg"; 
+      header.classList.add("scrolled"); call.src = "./images/call-color.svg";
     } else { 
-      header.classList.remove("scrolled"); logo.src = "./images/logo-white.svg"; 
+      header.classList.remove("scrolled"); logo.src = "./images/logo-white.svg";
+      header.classList.remove(".scrolled"); call.src = "./images/call.svg"; 
     }});
 });
 
