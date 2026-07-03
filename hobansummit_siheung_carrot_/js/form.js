ad201e3b15fda643b2b07d198d8f8a12acc8a27d
bunyangpro <bunyangpro2@gmail.com>
2026-07-03 16:41:06 +0900

    시흥호반써밋3 토스-당근 교체 및 수정

---
+++ hobansummit_siheung_carrot_/js/form.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // 폼 제출 시 시트로 전달
+
+  const forms = document.querySelectorAll("#reserveForm");
+
+  forms.forEach(function (form) {
+
+    form.addEventListener("submit", function (e) {
+
+      e.preventDefault();
+
+      const scriptURL = "https://script.google.com/macros/s/AKfycbzKtK2gcnoDu6XAhbRWG1Q69Vaof-bqeT-Be0pMU1hu95LnF-VcXTBCo6qdLvdis4g/exec";
+
+      const formData = new FormData(form);
+
+      fetch(scriptURL, {
+
+        method: "POST",
+        body: formData,
+        mode: "no-cors"
+
+      })
+      .then(() => {
+
+        form.reset();
+        window.location.href = "./complete/";
+
+      })
+      .catch((error) => {
+
+        console.error(error);
+        alert("전송 중 오류가 발생했습니다.");
+        
+      });
+    });
+  });
+});
