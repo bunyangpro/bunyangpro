dbe3b9c8fee36ade79f9300c8670251130f3eb83
bunyangpro <bunyangpro2@gmail.com>
2026-07-14 10:23:11 +0900

    부산 해링턴 마레 수정

---
+++ busan_mare/js/header.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+   // 헤더 스크롤 이벤트 
+  const header = document.getElementById("siteHeader");
+  const logo = document.querySelector(".header-logo");
+  const call = document.querySelector(".header-tel img");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+      header.classList.add("scrolled");
+      logo.src = "./images/logo-color.png";
+      call.src = "./images/call-color.svg";
+    } else {
+      header.classList.remove("scrolled");
+      logo.src = "./images/logo-white.png";
+      call.src = "./images/call.svg"
+    }
+  });
+});
+
