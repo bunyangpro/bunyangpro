faabe54a8937540944f0471ab63a942857dcce37
bunyangpro <bunyangpro2@gmail.com>
2026-08-14 11:31:26 +0900

    이영섭 본부장 시흥 호반써밋 - 당근 -> 언론 랜딩 복제

---
+++ hobansummit_siheung_unlone/js/header.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+   // 헤더 스크롤 이벤트 
+  const header = document.getElementById("siteHeader");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
+  const logo = document.querySelector(".header-logo");
+  const call = document.querySelector(".header-tel img");
+  
+  window.addEventListener("scroll", () => { 
+    if (window.scrollY > 60) { 
+      header.classList.add("scrolled"); logo.src = "./images/logo-color.png"; 
+      header.classList.add("scrolled"); call.src = "./images/call-color.svg";
+    } else { 
+      header.classList.remove("scrolled"); logo.src = "./images/logo-white.png"; 
+      header.classList.remove("scrolled"); call.src = "./images/call.svg";
+    }});
+});
+
